a1d96eab6c0b9b46b69ad99b5c58a55895348553
HuRuuCorporations <HuRuuCorporations@192.168.0.14>
2019-04-08 18:41:17 -0700

    Added Temp TestCases

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csc335-project6-p5-ryanluu52-dennyho
+
+
---
+++ src/ReversiTestCases.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class ReversiTestCases {
+	
+	@Test 
+	void testServer() {
+		
+	}
+	
+	@Test 
+	void testClient() {
+		
+	}
+	
+	@Test 
+	void testNetworkSetup() {
+		
+	}
+
+}
